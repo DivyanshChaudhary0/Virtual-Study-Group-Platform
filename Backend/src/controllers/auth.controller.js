52d880aff2e468c1a765c599e6d0b8b371eb2a2e
Divyansh chaudhary <hypr123890@gmail.com>
2025-04-26 11:07:03 +0530

    feat: remove unused logout function and clean up profile response

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -81,20 +81,15 @@ const login = async function(req,res){
     }
 }
 
-const logout = async function(req,res){
-    try{
-        
-    }
-    catch(err){
-        res.status(500).json({
-            message: err.message
-        })
-    }
-}
 
 const profile = async function(req,res){
     try{
+        const user = req.user;
 
+        res.status(200).json({
+            message: "Profile fetched ",
+            user
+        })
     }
     catch(err){
         res.status(500).json({
